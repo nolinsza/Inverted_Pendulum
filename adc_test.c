6b33d56c36f83990aa381eba3447f9ba189c4c69
nolinsza <135283067+nolinsza@users.noreply.github.com>
2026-03-30 17:38:07 -0400

    Update adc_test.c

--- adc_test.c
+++ adc_test.c
@@ -1,18 +1,55 @@
 #include <stdio.h>
 #include <stdint.h>
-#include "driver/adc.h"
 #include "esp_adc_cal.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include <esp_adc/adc_oneshot.h>
 #include "esp_log.h"
 
+#define TAG "ADC_TEST"
+#define VOLTAGE_POS_15 3.0f
+#define VOLTAGE_NEG_15 0.75f
+#define ADC_TO_VOLTAGE 0.000805861f //adc to voltage converstion
+
+float deg_per_volt = 30 / (VOLTAGE_POS_15 - VOLTAGE_NEG_15);
+float offset = 15 - (VOLTAGE_POS_15 * deg_per_volt); 
+
+float angle adc_to_angle(int adc)
+{
+  //Convert adc to voltage 
+  float voltage = adc * ADC_TO_VOLTAGE;
+
+  //make sure voltage is in range 
+  if (voltage > VOLTAGE_POS_15)
+  {
+    voltage = VOLTAGE_POS_15;
+  }
+
+  if (voltage < VOLTAGE_NEG_15)
+  {
+    voltage = VOLTAGE_NEG_15;
+  }
+
+  //convert voltage to angle 
+  angle = voltage * deg_per_volt + offset; 
+
+  return angle; 
+
+
+}
+
+//Calibration characteristics
+esp_adc_cal_characteristics_t *adc_chars = NULL;
+
 void app_main(void)
 {
 
+  const int dt_us = 1000;             //set sampling frequency 
+  const float alpha = 0.2f;
+
   int adc_val;
   adc_oneshot_unit_handle_t adc_handle; 
-  int32_t angle_val; 
+  float angle_deg, angle_rad, last_angle_rad; 
 
 
   //Initialize ADC Oneshot Mode Driver on the ADC Unit 
@@ -23,7 +60,7 @@ void app_main(void)
 
   adc_oneshot_new_unit(&init_config, &adc_handle);
 
-  //Configure ADC Channel 7 w/ 12 bit resolution and 0-3.3 V 
+  //Configure ADC Channel 7 (pin 35) w/ 12 bit resolution and 0-3.3 V 
   adc_oneshot_chan_cfg_t config = {
       .bitwidth = ADC_BITWIDTH_12,                //12 bit resolution
       .atten = ADC_ATTEN_DB_11,                   //0-3.3 V 
@@ -31,34 +68,46 @@ void app_main(void)
 
   adc_oneshot_config_channel(adc_handle, ADC_CHANNEL_7, &config);
 
+  //initialize filter adc value
+  adc_oneshot_read(adc_handle, ADC_CHANNEL_7, &adc_val); 
+  float adc_filter = (float)adc_val; 
+
+  //need to initialize all value compute angle etc.
+
+  //initialze variable tracking the next update time 
+  int64_t next_time = esp_timer_get_time();
+
   //Loop to read the ADC value 
   while (1)
   {
+
+    //wait until next sample time 
+    while(esp_timer_get_time() < next_time)
+    {
+      //do nothing 
+    }
+
     //Read ADC Value 
     adc_oneshot_read(adc_handle, ADC_CHANNEL_7, &adc_val);
-    //Print ADC Value 
-    ESP_LOGI("ADC Value", "%d", adc_val);
 
-    //Convert Voltage to angle - *May want to adjust this for a range of say -5 to 5 degrees for stronger resolution for actualy implementation 
-    if (adc_val > 4060) //maximum range 
-    {
-      printf("above max range");
-    }
-    else if (adc_val > 2668)  //compute positive angle 
-    {
-      angle_val = (adc_val - 2668) * 5407 / 1000000;
-    }
-    else if (adc_val < 1210)  //compute negative angle 
-    {
-      angle_val = (adc_val - 2668) * 5169/100000; 
-    }
-    else
-    {
-      printf("below min range");
-    }
+    adc_filter = alpha * adc_val + (1.0f - alpha) * adc_filter;
 
-    //Delay 1 second
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+    ESP_LOGI(TAG, "Raw: %d", adc_val);
+
+
+    //Convert Voltage to angle
+    //angle_deg = adc_to_angle(adc_filtered);
+
+    //angle_deg to rad 
+
+    //compute angular velocity
+
+    //store the last angle value 
+    //last_angle_rad = angle_rad;
+
+    //Update the next sample time 
+    next_time += dt_us;
 
   }
   
