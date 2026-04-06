04ba218afb96b75df32e9f23609382e983b45305
nolinsza <135283067+nolinsza@users.noreply.github.com>
2026-04-05 22:38:26 -0400

    Update ADC_Continuous_Test.c

--- ADC_Continuous_Test.c
+++ ADC_Continuous_Test.c
@@ -1,10 +1,42 @@
 
+#define OVERSAMPLE_RATE 512 //Use the last 512 ADC values
+
+const int dt_us = 1000;
+
+uint16_t adc_buffer[OVERSAMPLE_RATE] = {0};
+uint32_t adc_sum = 0;
+uint16_t adc_average = 0;
+uint16_t index = 0;
 
 //Callback function for continuous ADC 
-static bool IRAM_ATTR on_conv_done(adc_continuous_handle_t haqndle, const adc_continuous_evt_data_t *edata, void *user_data) {
-    // Perform light processing, e.g., notify a task
-    // DO NOT use blocking code (like printf or vTaskDelay) here
-    return false; // return true if a high priority task was woken up
+static bool IRAM_ATTR s_conv_done_cb(adc_continuous_handle_t handle, const adc_continuous_evt_data_t *edata, void *user_data) {
+
+    //create a pointer to the buffer
+    uint8_t *buf = edata->conv_frame_buffer;   //point to the adc data 
+    uint32_t len = edata->size;                //number of bytes in the buffer
+
+    for (int i = 0; i<len; i+=2)                //2 bytes per sample 
+        {
+
+            //extract adc sample 
+            uint16_t sample = ((uint16_t)buf[i] | (buf[i+1] << 8)) & 0x0FFF;
+            
+            //Remove oldest value
+            adc_sum -= adc_buffer[index];
+
+            //Store new sample
+            adc_buffer[index] = sample;
+
+            //add the new sample 
+            adc_sum += sample;
+
+            //update the index 
+            index = (index + 1) & (OVERSAMPLE_RATE -1); //raps around same thing as (index + 1) % 512
+        }
+
+    //compute average
+    adc_average = adc_sum / OVERSAMPLE_RATE; 
+    
 }
 
 //Callback for a timer to run at 1 KHz 
@@ -25,7 +57,7 @@ void init_timer()
     gptimer_config_t timer_config = {
         .clk_src = GPTIMER_CLK_SRC_DEFAULT,
         .direction = GPTIMER_COUNT_UP,
-        .resolution_hz = 1000000, // 1 MHz = 1 tick = 1 us
+        .resolution_hz = dt_us, // 1 MHz -> 1 tick = 1 us
     };
 
     ESP_ERROR_CHECK(gptimer_new_timer(&timer_config, &timer));
@@ -37,7 +69,7 @@ void init_timer()
     ESP_ERROR_CHECK(gptimer_register_event_callbacks(timer, &cbs, NULL));
 
     gptimer_alarm_config_t alarm_config = {
-        .alarm_count = 1000,  // 1000 us = 1 kHz
+        .alarm_count = dt_us,
         .reload_count = 0,
         .flags.auto_reload_on_alarm = true,
     };
@@ -67,14 +99,14 @@ void app_main(void)
   adc_continuous_handle_t handle = NULL;
 
   adc_continuous_handle_cfg_t adc_config = {
-        .max_store_buf_size = 1024,            //storage size in bytes
-        .conv_frame_size = 256,                //define the number of bytes to process at one time (frame)
+        .max_store_buf_size = 512,            //storage size in bytes
+        .conv_frame_size = 512,                //define the number of bytes to process at one time (frame)
     };
 
   adc_continuous_new_handle(&adc_config, &handle);
 
   adc_continuous_config_t dig_cfg = {
-        .sample_freq_hz = 100000,              //set the sampling frequency - 100kHz
+        .sample_freq_hz = 1000000,              //set the sampling frequency - 1MHz
         .conv_mode = ADC_CONV_SINGLE_UNIT_1,   //unit 1 
     };
 
@@ -94,10 +126,10 @@ void app_main(void)
   adc_continuous_evt_cbs_t cbs = {
         .on_conv_done = s_conv_done_cb,
   };
-  adc_continuous_register_event_callbacks(handle, &cbs, NULL)
+  adc_continuous_register_event_callbacks(handle, &cbs, NULL);
 
   //start the ADC sampling
-  adc_continuous_start(handle)
+  adc_continuous_start(handle);
 
     // Create control task
     xTaskCreate(control_task, "control_task", 4096, NULL, 10, &control_task_handle);
